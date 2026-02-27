1ba4a9ef230ddc0e27d8a335748faddf5604f326
SuperBigPuff <2652080065@qq.com>
2026-02-27 21:50:34 +0800

    comment

--- src/site.config.ts
+++ src/site.config.ts
@@ -160,7 +160,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://comments-pe17d86ws-puffs-projects-979999e2.vercel.app/',
+    server: 'https://comments-fpjpnyc5h-puffs-projects-979999e2.vercel.app/',
     // Show meta info for comments
     showMeta: false,
     // Refer https://waline.js.org/en/guide/features/emoji.html
